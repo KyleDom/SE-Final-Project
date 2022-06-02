be2652adba5807ffc30844f8470cfd8e5378bc47
Lyzza Flores <89468418+liechee@users.noreply.github.com>
2022-06-02 14:38:43 +0800

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -2,4 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour{}
+public class Enemy : MonoBehaviour{
+    public int health; 
+    public int DamageToPlayer;
+    public int value;
+    public float moveSpeed;
+
+    //path 
+    private Transform[] path;
+    private int currentWaypoint;
+
+    public GameObject healthBarPrefab; 
+
+    void Start (){
+        path = GameManager.instance.enemyPath.waypoints;
+        //creating the health bar
+        Canvas canvas = FindObjectOfType<Canvas>();
+        GameObject healthBar = Instantiate(healthBarPrefab, canvas.transform);
+        healthBar.GetComponent<EnemyHealthBar>().Initialize(this);
+    }
+
+    void Update (){
+        Move();
+    }
+}
